f04ab9080353592e0ca3cde43918adcbe53307c3
Jose Miguel Vasquez <pixgel13040@gmail.com>
2025-06-13 16:51:53 -0500

    feat: Actualizar componentes y páginas para integrar nuevos datos de capítulos y mejorar el diseño

---
+++ src/const/capitulos.ts
@@ -0,0 +1,49 @@
+import type { Capitulo } from "../model/capitulo.model";
+
+export const capitulos: Capitulo[] = [
+        {
+        cap: 1,
+        sinopsis: "En este capítulo, exploraremos los colores que nos definen y cómo estos influyen en nuestra identidad. A través de imágenes y reflexiones, descubriremos la paleta única que cada uno de nosotros lleva dentro.",
+        video: "SlZ8xeEBJms",
+        image: "https://res.cloudinary.com/deghyfe80/image/upload/c_crop,ar_16:9/v1749850403/andr%C3%A9_1_vvjjrl.webp",
+        titulo: "colores del origen",
+        slug: "colores-del-origen",
+        color: "blue"
+    },
+    {
+        cap: 2,
+        sinopsis: "En este capítulo, nos adentraremos en el mundo de las imágenes que nos rodean. Veremos cómo estas imágenes no solo capturan momentos, sino que también cuentan historias y reflejan nuestra realidad.",
+        video: "SlZ8xeEBJms",
+        image: "https://res.cloudinary.com/deghyfe80/image/upload/c_crop,ar_16:9/v1749850403/Bianca_1_gd7sak.webp",
+        titulo: "sonidos de identidad",
+        slug: "sonidos-de-identidad",
+        color: "yellow"
+    },
+    {
+        cap: 3,
+        sinopsis: "En este capítulo, exploraremos el poder de las palabras y cómo estas pueden habitar en nosotros. Veremos cómo las palabras pueden ser herramientas de transformación y expresión personal.",
+        video: "SlZ8xeEBJms",
+        image: "https://res.cloudinary.com/deghyfe80/image/upload/c_crop,ar_16:9/v1749850403/kenji_1_pprlnj.webp",
+        titulo: "cuerpos en escena",
+        slug: "cuerpos-en-escena",
+        color: "white"
+    },
+    {
+        cap: 4,
+        sinopsis: "En este capítulo, nos enfocaremos en el ADN creativo que todos llevamos dentro. Veremos cómo nuestras experiencias y pasiones se entrelazan para formar nuestra esencia creativa.",
+        video: "SlZ8xeEBJms",
+        image: "https://res.cloudinary.com/deghyfe80/image/upload/c_crop,ar_16:9/v1749850403/David-3_oohwjd.webp",
+        titulo: "palabras que habitan",
+        slug: "palabras-que-habitan",
+        color: "red"
+    },
+    {
+        cap: 5,
+        sinopsis: "En este capítulo, reflexionaremos sobre el viaje de autodescubrimiento que hemos emprendido. Veremos cómo cada uno de nosotros es un lienzo en blanco, listo para ser pintado con nuestras experiencias y aprendizajes.",
+        video: "SlZ8xeEBJms",
+        image: "https://images.ctfassets.net/hrltx12pl8hq/5TwlZb9X6z7KdFWx3I0wUd/32c5d11dd62e68bfd13ba10975043a9a/Dad-Thumbnail.jpg",
+        titulo: "adn creativo",
+        slug: "adn-creativo",
+        color: "green"
+    }
+    ]
--- src/model/capitulo.model.ts
+++ src/model/capitulo.model.ts
@@ -1,5 +1,9 @@
 export interface Capitulo {
     cap: number
+    image: string
     titulo: string
     sinopsis: string
+    color: string
+    slug: string
+    video: string
 }
